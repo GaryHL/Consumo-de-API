659534fbd7e284a7edbb2c3fbef7ab976c25823d
Gary HL <103480668+GaryHL@users.noreply.github.com>
2022-06-24 08:02:11 +0200

    Ultimos detalles

--- Movies app/script.js
+++ Movies app/script.js
@@ -1,28 +1,3 @@
-
-// modelo para explicar
-// const cargarPelis = async()=> {
-//     try{
-//         const respuesta = await fetch("https://api.themoviedb.org/3/movie/550?api_key=745780471818b1c222d7aa3ae617451f");
-//         console.log(respuesta);
-
-//         if(respuesta.status === 200){const datos = await respuesta.json();
-//         console.log(datos.title)
-
-//         }else if(respuesta.status === 401){
-//             console.log('Pusiste la llave mal');
-//         } else if(respuesta.status === 404){
-//             console.log('La pelicula que buscas no existe');
-//         } else {
-//             console.log('Hubo un error y no sabemos que paso');
-//         }
-//         }
-//     catch(error){
-//         console.log(error);
-//     }
-// }
-// cargarPelis()
-
-
 const cargarPelis = async()=>{
     try{const respuesta = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${pagina}");
     if(respuesta.status=200){
@@ -32,7 +7,7 @@ const cargarPelis = async()=>{
 			datos.results.forEach(pelicula => {
 				peliculas += `
 					<div class="pelicula">
-						<img class="poster" src="https://image.tmdb.org/t/p/w200/${pelicula.poster_path}">
+						<img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
                             <h3 class="titulo">${pelicula.title}</h3>`
                             if(pelicula.vote_average>7.4){
                             peliculas+=   `<div class="cualification verde">${pelicula.vote_average}</div>`
